010328eb4f905de3965937805ba2e5025d7bd62c
Peter Smith <113214079+pesmith0@users.noreply.github.com>
2023-10-02 21:36:33 -0700

    added WIP bitfielder_parser.py; doesn't yet work
    This is a python program that uses the python tokenizer module and Lark to tokenize an input file, convert it into a string Lark can parse, parse it, and print it. Currently dealing with an issue where the tokenizer mysteriously removes spacebars.

---
+++ bitfielder_parser.py
@@ -0,0 +1,69 @@
+from tokenize import tokenize, INDENT, DEDENT, NEWLINE, ENCODING
+from io import BytesIO
+from lark import Lark
+
+def tokenize_bitfielder(s):
+    """
+    Takes a string. Replaces certain tokens with ones that Lark can parse. Returns the modified string.
+    """
+    result = []
+    g = tokenize(BytesIO(s.encode('utf-8')).readline)  # tokenize the string
+    for token_type, token_value, _begin_pos, _end_pos, _line_content in g:
+        if token_type == INDENT:
+            result.append("{{{")
+        elif token_type == DEDENT:
+            result.append("}}}")
+        elif token_type == NEWLINE: # there is a separate token for solitary newlines called "NL"
+            result.append(";\n")
+        elif token_type == ENCODING:
+            pass
+        else:
+            result.append(token_value)
+        
+    return "".join(result)
+
+lark_parser = Lark(r"""
+    program : fixed_int_stmt [prefix_stmt] stmt*
+                   
+    IDENTIFIER : /[A-Za-z_][A-Za-z_0-9]*/
+                   
+    fixed_int_stmt : IDENTIFIER name
+    
+    name : IDENTIFIER
+                   
+    prefix_stmt : "prefix" IDENTIFIER
+    
+    stmt : property_stmt | super_property
+    
+    property_stmt : "property" name bits
+    bits : SIGNED_NUMBER
+                   
+    super_property : "property" name", prefix" name "{{{" property_list
+    property_list : property_stmt property_list | property_stmt "}}}"
+                   
+    %import common.SIGNED_NUMBER
+    %import common.WS
+    %ignore WS
+    %ignore ";"
+
+    """, start='program')
+
+
+if __name__ == '__main__':
+    import sys
+
+    filename = sys.argv[1]
+    f = open(filename, "r")
+    s = f.read()
+
+    modded_string = tokenize_bitfielder(s)
+    
+    print("modded_string:\n%r" % modded_string)
+
+    lark_output = lark_parser.parse(modded_string)
+
+    print("%r" % lark_output)
+
+    print("\npretty:")
+
+    print(lark_output.pretty())
